5b9db65a363a20a135bd5f6e0a238a8d440c96bb
Wellerson Ferreira <117539408+WellersonFerreir@users.noreply.github.com>
2023-02-20 23:53:17 -0300

    Add files via upload

---
+++ assets/js/questions.js
@@ -0,0 +1,42 @@
+export default [
+  {
+    question:" <img src='../assets/img/tartaruga.svg' alt='animal marinho'> <span><br>Tenho casco e passo grande parte da vida na água.</span>",
+    answers: [
+      { option: "Tartaruga", correct: true },
+      { option: "Jabuti", correct: false },
+      { option: "Cagado", correct: false },
+    ],
+  },
+  {
+    question: "<img src='../assets/img/ra.svg' alt='animal semi-acuatico'> <span><br>Gosto de lugares com água e pulo muito alto.</span>",
+    answers: [
+      { option: "Sapo", correct: false },
+      { option: "Rã", correct: true },
+      { option: "Salamandra", correct: false },
+    ],
+  },
+  {
+    question: "<img src='../assets/img/camaleao.svg' alt='animal terrestre'> <span><br>Alguns animaizinhos da minha espécie podem mudar de cor.</span>",
+    answers: [
+      { option: "Iguana", correct: false },
+      { option: "Lagarto", correct: false },
+      { option: "Camaleao", correct: true },
+    ],
+  },
+  {
+    question: "<img src='../assets/img/lebre.svg' alt='animal terrestre'> <span><br>Sou fofinho, ando saltando e vivo sozinho.</span>",
+    answers: [
+      { option: "Coelho", correct: false },
+      { option: "Canguru", correct: false },
+      { option: "Lebre", correct: true },
+    ],
+  },
+  {
+    question: "<img src='../assets/img/lula.svg' alt='animal marinho'> <span><br>Eu vivo na água salgada e tenho a cabeça achatada.</span>",
+    answers: [
+      { option: "Polvo", correct: false },
+      { option: "Kraken", correct: false },
+      { option: "Lula", correct: true },
+    ],
+  },
+];
